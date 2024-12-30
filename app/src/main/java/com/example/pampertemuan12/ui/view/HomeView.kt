b95165fddb509f6a378ac770cf869849128dcce4
ichsanHPM <ichsanhpm@gmail.com>
2024-12-30 12:31:55 +0700

    Menambahkan object DestinasiHome, fungsi HomeScreen, fungsi HomeStatus, fungsi OnLoading,fungsi OnError,fungsi MhsLayout

--- app/src/main/java/com/example/pampertemuan12/ui/view/HomeView.kt
+++ app/src/main/java/com/example/pampertemuan12/ui/view/HomeView.kt
@@ -1,25 +1,178 @@
 package com.example.pampertemuan12.ui.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pampertemuan12.R
 import com.example.pampertemuan12.model.Mahasiswa
+import com.example.pampertemuan12.ui.Navigation.DestinasiNavigasi
+import com.example.pampertemuan12.ui.customwidget.CustomeTopAppBar
+import com.example.pampertemuan12.ui.viewmodel.HomeUiState
+import com.example.pampertemuan12.ui.viewmodel.HomeViewModel
+import com.example.pampertemuan12.ui.viewmodel.PenyediaViewModel
 
+
+object DestinasiHome: DestinasiNavigasi{
+    override val route = "home"
+    override val titleRes = "Home Mhs"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry:()->Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick:(String)->Unit = {},
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CustomeTopAppBar(
+                title = DestinasiHome.titleRes,
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior,
+                onRefresh = {
+                    viewModel.getMhs()
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add Kontak")
+            }
+        }
+    ) { innerpadding->
+        HomeStatus(
+            homeUiState = viewModel.mhsUIState,
+            retryAction = {viewModel.getMhs()}, modifier = Modifier.padding(innerpadding),
+            onDetailClick = onDetailClick, onDeleteCLick = {
+                viewModel.deleteMhs(it.nim)
+                viewModel.getMhs()
+            }
+        )
+    }
+}
+
+@Composable
+fun HomeStatus(
+    homeUiState: HomeUiState,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDeleteCLick: (Mahasiswa) -> Unit = {},
+    onDetailClick: (String) -> Unit
+){
+    when(homeUiState){
+        is HomeUiState.Loading-> OnLoading(modifier = modifier.fillMaxSize())
+        is HomeUiState.Success->
+            if (homeUiState.mahasiswa.isEmpty()){
+                return Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+                    Text(text = "Tidak ada data Kontak")
+                }
+            }else{
+                MhsLayout(
+                    mahasiswa = homeUiState.mahasiswa, modifier = modifier.fillMaxWidth(),
+                    onDetailClick = {
+                        onDetailClick(it.nim)
+                    },
+                    onDeleteCLick = {
+                        onDeleteCLick(it)
+                    }
+                )
+            }
+        is HomeUiState.Error-> OnError(retryAction,modifier = modifier.fillMaxSize())
+    }
+}
+
+@Composable
+fun OnLoading(modifier: Modifier = Modifier){
+    Image(
+        modifier = Modifier.size(200.dp),
+        painter = painterResource(R.drawable.loading),
+        contentDescription = stringResource(R.string.loading)
+    )
+}
+
+@Composable
+fun OnError(retryAction:()->Unit, modifier: Modifier = Modifier){
+    Column(
+        modifier = Modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_connection_error), contentDescription = ""
+        )
+        Text(text = stringResource(R.string.loading_failed), modifier = Modifier.padding(16.dp))
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+        }
+    }
+}
+
+@Composable
+fun MhsLayout(
+    mahasiswa: List<Mahasiswa>,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Mahasiswa)->Unit,
+    onDeleteCLick: (Mahasiswa) -> Unit = {}
+){
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(mahasiswa){kontak->
+            MhsCard(
+                mahasiswa = kontak,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { onDetailClick(kontak) },
+                onDeleteCLick = {
+                    onDeleteCLick(kontak)
+                }
+            )
+        }
+    }
+}
 
 @Composable
 fun MhsCard(
