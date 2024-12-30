dfd67afd025c37ea4bda5d79a0f8c5e2f35a6fbc
ichsanHPM <ichsanhpm@gmail.com>
2024-12-30 07:46:11 +0700

    Menambahkan data class dan fungsi InsertUiEvent

--- app/src/main/java/com/example/pampertemuan12/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pampertemuan12/ui/viewmodel/InsertViewModel.kt
@@ -2,6 +2,28 @@ package com.example.pampertemuan12.ui.viewmodel
 
 import com.example.pampertemuan12.model.Mahasiswa
 
+data class InsertUiState(
+    val insertUiEvent : InsertUiEvent = InsertUiEvent()
+)
+
+data class InsertUiEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jeniskelamin: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+
+fun InsertUiEvent.toMhs(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jeniskelamin = jeniskelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
 fun Mahasiswa.toUiStateMhs(): InsertUiState = InsertUiState(
     insertUiEvent = toInsertUiEvent()
 )
